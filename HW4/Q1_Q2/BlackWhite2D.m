d06b3e08ba0ae22f80a72adbe85956704017bd0c
Qianqian Fang <fangqq@gmail.com>
2024-11-21 17:50:27 -0500

    Add sample files for Homework 4

---
+++ HW4/Q1_Q2/BlackWhite2D.m
@@ -0,0 +1,16 @@
+%
+% please write help info for this class, see example at
+% https://github.com/NeuroJSON/jsonlab/blob/master/jdict.m
+%
+
+classdef BlackWhite2D
+
+  properties
+
+  end
+
+  methods
+
+  end
+
+end
---
+++ HW4/Q3/README.txt
@@ -0,0 +1,2 @@
+# A MATLAB Pong Game
+
